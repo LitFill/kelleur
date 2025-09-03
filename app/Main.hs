3511118d60b40c8cacdff44d4f1b738ed62ad3b5
LitFill <marrazzy54@gmail.com>
2025-09-03 22:36:44 +0700

    refactor(Main): rapihkan cekN

--- app/Main.hs
+++ app/Main.hs
@@ -44,9 +44,11 @@ cek s = do
         else "\x1b[31mnooooooo Wrong 😢\x1b[0m"
 
 cekN :: Int -> IO ()
-cekN n = case find (\s -> s.number == n && not (null s.desc)) solutions of
-    Just s -> cek s
-    Nothing -> putStrLn $ "Problem " ++ show n ++ " is not solved yet."
+cekN n = case find go solutions of
+    Just s  -> cek s
+    Nothing -> printf "Problem %s is not solved yet." n
+  where
+    go s = s.number == n && not (null s.desc)
 
 handleArgs :: [String] -> IO ()
 handleArgs = \case
